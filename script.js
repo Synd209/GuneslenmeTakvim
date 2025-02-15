746ae8ef8d6b409abd24c012d7bda6a0a2eb530f
Glitch (glitch-blank-website) <none>
2025-02-15 20:54:08 +0300

    first version kinda

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//#region Calculations
 function dayOfYear(year, month, day) {
     let dt = new Date(year, month - 1, day);
     let start = new Date(year, 0, 0);
@@ -60,54 +61,237 @@ function sunAltitude(date, time, latitudeDeg, longitudeDeg, timezoneOffset) {
     return altDeg + refractionDeg;
 }
 
+function getSolarDeclination(date){
+  /* Returns the declination to set the rotation of the earth. */
+  return 23.44 * Math.sin( (360 / 365.25) * getDayOfYear(date) * Math.PI/180 )
+}
 
-// Get elements inside variables
-let full_date = document.getElementById("full-date");
-let latitude = document.getElementById("latitude");
-let longitude = document.getElementById("longitude");
-let chart;
+function getDayOfYear(date){
+  var start = new Date(date.getFullYear(), 0, 0);
+  var diff = date - start;
+  var oneDay = 1000 * 60 * 60 * 24;
+  var dayOfYear = Math.floor(diff / oneDay);
+  return dayOfYear + 284
+}
+//#endregion
+
+//#region Formating
+function format(time_obj, display) {
+  /* Formats datetime objects into a more readable format. */
+  // Display is either d (date) or t (time)
+
+  switch (display){
+    case 'd':
+      return `${time_obj.getFullYear()}-${(time_obj.getMonth() + 1).toString().padStart(2, '0')}-${time_obj.getDate().toString().padStart(2, '0')}`
+
+    case 't':
+      if(time_obj.getSeconds() == 0)
+        return `${time_obj.getHours()}:${time_obj.getMinutes().toString().padStart(2, '0')}`
+
+      else
+        return `${time_obj.getHours()}:${time_obj.getMinutes().toString().padStart(2, '0')}:${time_obj.getSeconds().toString().padStart(2, '0')}`
+  }
+
+}
+//#endregion
+
+//#region Global vars
+const full_date = document.getElementById("full-date");
+const latitude = document.getElementById("latitude");
+const longitude = document.getElementById("longitude");
+const earth = document.getElementById("earth");
+const city = document.getElementById("city-input");
+const UTCBox = document.getElementById("UTC-box");
+
+let dayOfYear_var;
+let timezoneOffset;
+let daily_chart;
+let yearly_chart;
+//#endregion
+
+//#region Updating
+function getCoordinates() {
+  /* Enters the coor */
+  if (city.value.length <= 0){
+    document.getElementById("city-warn").style.display = "none";
+    return;
+  }
+  
+  fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${city.value}`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.length > 0) {
+        latitude.value = data[0].lat;
+        longitude.value = data[0].lon;
+        document.getElementById("city-warn").style.display = "none";
+        updateAll()
+      } else {
+        document.getElementById("city-warn").style.display = "block";
+      }
+    })
+    .catch(error => console.error(error));
+}
 
 function displaySunDegree() {
   let date = new Date(full_date.value)
-  
-  
-  let timezoneOffset = -date.getTimezoneOffset() / 60;
-  let maxDegree = 0;
-  let maxTime = new Date();
-  
-  // Generate data points
+  let maxDegree = 0, minDegree = 90;
+  let maxTime = new Date(), minTime = new Date();
+  let fiftyStart = null, fiftyEnd = null;
+  let sunrise = null, sunset = null;
+
+  // Get timezone
+  function convertH2M(timeInHour){
+    let timeParts = timeInHour.split(":");
+    if(timeParts.length == 1)
+      return Number(timeParts[0]) * 60;
+
+    return Number(timeParts[0]) * 60 + Number(timeParts[1]);
+  }
+
+  let first = UTCBox.value.at(0)
+  let temp = (first == '-'? -1 : 1) * convertH2M(UTCBox.value.slice((first == '-' || first == '+')? 1 : 0));
+  timezoneOffset = temp / 60;
+
+  let hour = Math.trunc(Math.abs(timezoneOffset)).toString().padStart(2, '0');
+  let minute = (Math.abs(temp) % 60).toString().padStart(2, '0');
+
+  if(hour == "NaN" || minute == "NaN") {
+    hour = "00";
+    minute = "00";
+    timezoneOffset = 0;
+  }
+
+  UTCBox.value = `${timezoneOffset >= 0? '+': '-'}${hour}:${minute}`
+
+  // Generate data points 
   const yValues = [];
   
+  let pastAlt = null;
   for (let minute = 0; minute <= 1440; minute++) {
       let time = new Date(date.getFullYear(), date.getMonth(), date.getDate(), Math.floor(minute / 60), minute % 60, 0);
       let alt = sunAltitude(date, time, latitude.value, longitude.value, timezoneOffset);
       
+      // Modify alt
+      if (alt < 0)
+        alt = 0;
+      
+      yValues.push(alt)
+
+      // Get min and max degree
       if(alt > maxDegree){
         maxDegree = alt;
         maxTime = time
       }
-    
-      if (alt > 0)
-        yValues.push(alt);
-      
-      else
-        yValues.push(0)
+
+      if(alt < minDegree){
+        minDegree = alt;
+        minTime = time
+      }
+
+      // to avoid comparing nothing
+      if (pastAlt === null){
+        pastAlt = alt
+        continue
+      }
+
+      // Get fify start and end
+      if(alt >= 50 && pastAlt <= 50){
+        fiftyStart = time
+      }
+
+      if(alt <= 50 && pastAlt >= 50){
+        fiftyEnd = time
+      }
+
+      // Get sunrise and sunset
+      if(alt > 0 && pastAlt <= 0){
+        sunrise = time
+      }
+
+      if(alt <= 0 && pastAlt > 0){
+        sunset = time
+      }
+
+      pastAlt = alt
   }
 
-  chart.data.datasets[0].data = yValues; // Update dataset
-  chart.update();
+  daily_chart.data.datasets[0].data = yValues; // Update dataset
+  daily_chart.update();
   
   // Update res-box
   let resBox = document.getElementById("res-box");
-  resBox.innerHTML = `Güneşin en dik geldiği saat ${Math.round(maxDegree * 1000) / 1000} derece ile ${maxTime.getHours()}:${maxTime.getMinutes()}`
+
+  // If the sun never gets there then we leave a space
+  resBox.innerHTML = ((maxDegree <= 0)? "" : `Güneşin en dik geldiği saat ${String(Math.round(maxDegree * 100) / 100).replace('.', ',')} derece ile ${format(maxTime, 't')}.` + "<br>") + 
+  ((minDegree > 0)?
+    "Bugün gün boyu güneş göreceksiniz." : 
+    (maxDegree <= 0)?
+      "Bugün tek günışığı sensin :)":
+      (sunrise > sunset)?  
+        `Güneş, ${format(sunset, 't')} ile batacak ve ${format(sunrise, 't')} ile tekrar doğacak.`:
+        `Güneş saat ${format(sunrise, 't')} ile doğacak ve saat ${format(sunset, 't')} ile batacak.`) +
+  "<br>" +
+  ((maxDegree <= 50)?
+    "Maalesef bugün D vitamininden mahrum kalacaksın." :
+    `${format(fiftyStart, 't')} ile ${format(fiftyEnd, 't')} saatleri arasında maksimum UVB ışını alabilirsiniz.`)  
 }
 
-function initialize() {
-  const xValues = [];
+function changeEarthRotation(){
+  let date = new Date(full_date.value)
   
-  const canvas = document.getElementById('res-canvas').getContext('2d');
-  
-  chart = new Chart(canvas, {
+  earth.style.transform = `rotate(${getSolarDeclination(date)}deg)`;
+  dayOfYear_var = dayOfYear(date.getFullYear(), date.getMonth() + 1, date.getDate());
+}
+
+function updateYearChart() {
+  console.log("Entered")
+  const yValues = [];
+  let date = new Date(full_date.value);
+  let year = date.getFullYear();
+  let lat = latitude.value;
+
+  let maxVal = isLeapYear(year)? 367 : 366; // Plus one as it stars from day 1
+  for(let day = 1; day < maxVal; day++) {
+    let solarDec = getSolarDeclination(new Date(year, 0, day));
+    let maxSolarDegree = 90 - Math.abs(solarDec - lat);
+    
+    if(maxSolarDegree < 0) maxSolarDegree = 0;
+
+    yValues.push(maxSolarDegree);
+  }
+
+  yearly_chart.data.datasets[0].data = yValues; // Update dataset
+  yearly_chart.update();
+}
+
+function updateAll(){
+  displaySunDegree();
+  changeEarthRotation();
+  updateYearChart();
+}
+//#endregion
+
+function showLabels() {
+  return window.innerWidth > 500;
+}
+
+function isLeapYear(year) {
+  return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+function numToMonth(num, reverse=false) {
+  const months = [
+    "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+    "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"
+  ];
+  return reverse? months.indexOf(num) : months[num];
+}
+
+//#region initialize
+function initialize() {  
+  const daily_canvas = document.getElementById('res-canvas').getContext('2d');
+
+  daily_chart = new Chart(daily_canvas, {
       type: 'line',
       data: {
           labels: Array.from({ length: 24 * 60 }, (_, i) => { // Generate time labels for every minute
@@ -116,48 +300,189 @@ function initialize() {
             return `${hours}:${minutes.toString().padStart(2, '0')}`; // Format as HH:MM
         }),
           datasets: [{
-              label: 'Dereceler',
-              data: Array.from({ length: 24 * 60 }, () => {return 0;}),
-              borderColor: 'yellow',
-              borderWidth: 4,
-              fill: true,
-              pointRadius: 0
+            label: "Güneşin geliş açısı",
+            data: Array.from({ length: 24 * 60 }, () => {return 0;}),
+            borderColor: 'yellow',
+            borderWidth: 4,
+            fill: true,
+            pointRadius: 0,
+            pointHitRadius: 15,
+            segment: {
+              borderColor: ctx => {
+                return ctx.p0.raw >= 50 ? 'lime' : 'grey';
+              }
+            }
           }]
       },
       options: {
-          responsive: false,
-          maintainAspectRatio: true,
-          scales: {
-            x: {
-              type: "category",
-              ticks: {
-                callback: function (value, index, values) {
-                  let hours = Math.floor(value / 60);  // Convert minutes to hours
-                  let minutes = value % 60;            // Get the remainder for minutes
-                  return minutes === 0 ? `${hours}:00` : null; // Only show hour labels
-
-                }
-              }
+        responsive: true,
+        maintainAspectRatio: true,
+        scales: {
+          x: {
+            title: {
+              display: showLabels(),
+              text: "Saat"
             },
-            y: {
-                min: 0,
-                max: 90
+            type: "category",
+            ticks: {
+              callback: function (value, index, values) {
+                let hours = Math.floor(value / 60);  // Convert minutes to hours
+                let minutes = value % 60;            // Get the remainder for minutes
+                return minutes === 0 ? `${hours}:00` : null; // Only show hour labels
+              }
+            }
+          },
+          y: {
+            title: {
+              display: showLabels(),
+              text: "Açı"
+            },
+            min: 0,
+            max: 90,
+            ticks: {
+              callback: function(value, index, values) {
+                return value + '°';
+              } 
             }
           }
+        },
+          
+        plugins: {
+          legend: {
+            display: false
+          },
+          tooltip: {
+            callbacks: {
+              label: function(context) {
+                return (Math.round(context.raw * 100) / 100 + '°').replace('.', ',');
+              }
+            }
+          }
+        }
       }
   });
 
+
+  const yearly_canvas = document.getElementById('year-canvas').getContext('2d');
+
+  yearly_chart = new Chart(yearly_canvas, {
+    type: 'line',
+    data: {
+        labels: Array.from({ length: 365 }, (_, i) => { // Generate time labels for every month
+          let date = new Date(1, 0, i+1)
+          return date.getDate() + ' ' + numToMonth(date.getMonth());
+      }),
+        datasets: [{
+          label: "Güneşin geliş açısı",
+          data: Array.from({ length: 365 }, () => {return 0;}),
+          borderColor: ctx => {
+            return (ctx.dataIndex == dayOfYear_var)? "purple" : "yellow";
+          },
+          borderWidth: 4,
+          fill: true,
+          pointRadius: ctx => {
+            return (ctx.dataIndex == dayOfYear_var)? 2 : 0;
+          },
+          pointHitRadius: 15,
+          
+        }]
+    },
+    options: {
+      onClick: function(event, elements) {
+
+        if (elements.length > 0) {
+          const element = elements[0]; // Get the clicked element
+          const dataIndex = element.index; // Get the data index of the clicked point
+          const xValue = yearly_chart.data.labels[dataIndex]; // Get the x-value (label)
+
+          const [day, month] = xValue.split(' ');
+          const year = new Date(full_date.value).getFullYear();
+          const numMonth = numToMonth(month, reverse=true);
+
+          const new_date = new Date(year, numMonth, day);
+          full_date.value = format(new_date, 'd');
+
+          dayOfYear_var = dayOfYear(year, parseInt(numMonth) + 1, parseInt(day));
+
+          updateAll();
+        }
+      },
+      responsive: true,
+      maintainAspectRatio: true,
+      scales: {
+        x: {
+          grid: { display: false },
+          offset: true,
+          title: {
+            display: true,
+            text: "Tarih"
+          },
+          type: "category",
+          ticks: {
+            callback: function (value, index, values) {
+              let date = new Date(1, 0, value)
+              return date.getDate() == 15 ? `${numToMonth(date.getMonth())}`: null; // Only show months
+            }
+          }
+        },
+        y: {
+          title: {
+            display: showLabels(),
+            text: "Açı"
+          },
+          min: 0,
+          max: 90,
+          ticks: {
+            callback: function(value, index, values) {
+              return value + '°';
+            } 
+          }
+        }
+      },
+        
+      plugins: {
+        legend: {
+          display: false
+        },
+        tooltip: {
+          callbacks: {
+            label: function(context) {
+              return (Math.round(context.raw * 100) / 100 + '°').replace('.', ',');
+            }
+          }
+        }
+      }
+    }
+});
   // Set default values for year and such
   const today = new Date()
-  full_date.value = `${today.getFullYear()}-${(today.getMonth() + 1).toString().padStart(2, '0')}-${today.getDate().toString().padStart(2, '0')}`
+  full_date.value = format(today, 'd');
+  let hourOffset = -today.getTimezoneOffset() / 60;
+  dayOfYear_var = dayOfYear(today.getFullYear(), today.getMonth() + 1, today.getDate());
 
+  let hour = Math.floor(Math.abs(hourOffset)).toString().padStart(2, '0')
+  let minute = (Math.abs(today.getTimezoneOffset()) % 60).toString().padStart(2, '0')
+
+  UTCBox.value = `${hourOffset >= 0? '+': '-'}${hour}:${minute}`
+  timezoneOffset = hourOffset;
+
+  // In case we cant get the position
+  latitude.value = 0
+  longitude.value = 0
+  changeEarthRotation()
+
+  
   // Set default values for coordinates
-  navigator.geolocation.getCurrentPosition(setPos);
+  navigator.geolocation.getCurrentPosition(setPos, fail);
 
   function setPos(pos) {
     latitude.value = pos.coords.latitude
     longitude.value = pos.coords.longitude
-    displaySunDegree()
+    updateAll()
+  }
+
+  function fail() {
+    updateAll();
   }
 }
 
@@ -165,10 +490,23 @@ function initialize() {
 initialize()
 
 // Add listeners for losing focus and update
-full_date.addEventListener("change", displaySunDegree)
-latitude.addEventListener("change", displaySunDegree)
-longitude.addEventListener("change", displaySunDegree)
+full_date.addEventListener("change", updateAll);
+latitude.addEventListener("change", updateAll);
+longitude.addEventListener("change", updateAll);
+UTCBox.addEventListener("change", displaySunDegree);
 
+city.addEventListener("change", getCoordinates);
 
+// 
+window.addEventListener("resize", () => {
+  const showLabel = showLabels();
+  daily_chart.options.scales.x.title.display = showLabel;
+  daily_chart.options.scales.y.title.display = showLabel;
+  daily_chart.update();
 
+  yearly_chart.options.scales.x.title.display = showLabel;
+  yearly_chart.options.scales.y.title.display = showLabel;
+  yearly_chart.update();
+});
 
+//#endregion
